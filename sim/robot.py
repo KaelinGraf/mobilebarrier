8f833b5aeb0a00eb7d890f80956443f028012898
KaelinGraf <kgra470@aucklanduni.ac.nz>
2026-04-30 17:06:52 +1200

    added robot as abstract class

--- sim/robot.py
+++ sim/robot.py
@@ -1,2 +1,10 @@
-class Robot:
-    def __init__(self,)
+from physical_models.params import Params
+from abc import ABC,abstractmethod
+
+class Robot(ABC):
+    def __init__(self,params:Params)
+        pass 
+
+    @abstractmethod
+    def step(self):
+        pass
